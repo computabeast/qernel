123575e9bd244869640db4dc1cfefffe3cf1366f
Seun Omonije <seun@aya.yale.edu>
2025-08-21 00:19:26 -0700

    proper setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+Setup script for qernel package.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="qernel",
+    version="0.1.0",
+    author="Seun Omonije",
+    description="An agentic, virtual kernel for efficient programming of quantum systemsm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.9",
+    install_requires=requirements,
+    extras_require={},
+)
